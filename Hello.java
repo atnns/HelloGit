398c264be090e685f0d971320ce9bfd6e1038de1
atnns <aho5@huskers.unl.edu>
2026-08-29 20:06:55 +0000

    Sat, Aug 29, 2026, 3:06 PM -05:00

---
+++ Hello.java
@@ -0,0 +1,16 @@
+/**
+ * Author: Athen Ho and Bryan Mellado
+ * Date: 08/29/2026
+ *
+ * Java Hack 1
+ *
+ */
+
+public class Hello {
+
+    public static void main(String args[]) {
+        System.out.println("Athen Ho!");
+        System.out.println("Computer SCience!");
+        
+    }
+}
