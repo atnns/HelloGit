831b9874cb56dbeb5fb2c3bbbf43cc7133dc2941
atnns <athenho2008@gmail.com>
2026-08-29 16:18:20 -0500

    added name and major

--- Hello.java
+++ Hello.java
@@ -9,8 +9,8 @@
 public class Hello {
 
     public static void main(String args[]) {
-        System.out.println("Athen Ho!");
+        System.out.println("Athen Ho.");
         System.out.println("Computer SCience!");
-        
+
     }
 }
