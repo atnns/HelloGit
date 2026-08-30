644665f17988ac659b14be3f563610f959563cf0
bmel357 <04724191@nebraska.edu>
2026-08-29 19:33:06 -0500

    added my name and major to the output

--- Hello.java
+++ Hello.java
@@ -11,6 +11,8 @@ public class Hello {
     public static void main(String args[]) {
         System.out.println("Athen Ho.");
         System.out.println("Computer SCience!");
+        System.out.println("Bryan Mellado");
+        System.out.println("Robotics Engineering");
 
     }
 }
